9ab441255f82ae67f0a898ed2f0f7c45084be15e
MicahJank <MicahJank@users.noreply.github.com>
2019-09-06 13:27:23 -0500

    began working on the state of the app.js class component added a componentDidMoutn and made some axios calls to the github api to get the user data

--- github-usercard/src/App.js
+++ github-usercard/src/App.js
@@ -1,9 +1,33 @@
 import React from 'react';
 
-function App() {
-  return (
-    
-  );
+import axios from 'axios';
+
+class App extends React.Component {
+  // since we dont need to use props we dont need a constructor or super and can set initial state this way
+  state = {
+    user: [],
+    followers: []
+  }
+
+  componentDidMount() {
+    axios.get(`https://api.github.com/users/MicahJank`)
+      .then(res => {
+        this.setState({user: res.data})
+        return res.data.followers_url;
+      })
+      .then(followersURL => {
+        axios.get(followersURL)
+          .then(res => this.setState({followers: res.data}))
+      })
+      .catch(err => alert(err));
+  }
+
+  render() {
+    console.log(this.state)
+    return (
+      <div>Hello</div>
+    );
+  }
 }
 
 export default App;
