fe84e04c2bc14e78911ee6ad500ece3b9fc3b870
MicahJank <MicahJank@users.noreply.github.com>
2019-09-06 20:18:52 -0500

    created a follower card component for the individual followers

---
+++ github-usercard/src/components/FollowerCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const FollowerCard = ( { user } ) => {
+
+    return (
+        <div>
+            <img src={user.avatar_url} alt='current user profile' />
+            <h3>{user.name}</h3>
+            <p>{user.bio}</p>
+            <h4>Currently followed by {user.followers} people</h4>
+            <h4>Currently following {user.following} people</h4>
+        </div>
+    );
+}
+
+export default FollowerCard;
---
+++ github-usercard/src/components/FollowersList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const FollowersList = () => {
+
+    return (
+        <div>FollowersList</div>
+    );
+}
+
+export default FollowersList;
