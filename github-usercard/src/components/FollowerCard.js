d5b8840fb42ec405249eb00efb5178bcc8c08fa0
MicahJank <MicahJank@users.noreply.github.com>
2019-09-07 16:57:26 -0500

    styled follower cards a little more

--- github-usercard/src/components/FollowerCard.js
+++ github-usercard/src/components/FollowerCard.js
@@ -4,6 +4,7 @@ import { makeStyles, Card, CardActionArea, CardActions, CardContent, CardMedia, 
 
 const useStyles = makeStyles({
     card: {
+        marginTop: 25,
         maxWidth: 245,
     },
     media: {
@@ -18,12 +19,7 @@ const FollowerCard = ( { follower } ) => {
             <CardActionArea>
                 <CardMedia component='img' image={follower.avatar_url} alt='current follower profile' title='current follower profile' />
                 <CardContent>
-                    <Typography gutterBottom variant='h5' component='h2'>{follower.name}</Typography>
-                    <Typography variant='body2' color='textSecondary' component='p'>{follower.bio}</Typography>
-                </CardContent>
-                <CardContent>
-                    <Typography variant='subtitle1' component='sub'>Followed by {follower.followers} people</Typography>
-                    <Typography variant='subtitle1' component='sub'>Following {follower.following} people</Typography>
+                    <Typography gutterBottom variant='h5' component='h2'>{`@${follower.login}`}</Typography>
                 </CardContent>
             </CardActionArea>         
         </Card>
