e7d37e10c2ca7db3a03154a012958bcdaed2095f
MicahJank <MicahJank@users.noreply.github.com>
2019-09-06 20:56:49 -0500

    decided against using shards as the styling was a pain, instead going to try material ui

--- github-usercard/src/components/UserCard.js
+++ github-usercard/src/components/UserCard.js
@@ -1,16 +1,23 @@
 import React from 'react';
 
+import {Card, CardHeader, CardTitle, CardImg, CardBody, CardFooter,Button} from "shards-react";
 
 const UserCard = ( { user } ) => {
 
     return (
-        <div>
-            <img src={user.avatar_url} alt='current user profile' />
-            <h3>{user.name}</h3>
-            <p>{user.bio}</p>
-            <h4>Currently followed by {user.followers} people</h4>
-            <h4>Currently following {user.following} people</h4>
-        </div>
+        <Card small={true}>
+            <CardHeader>{user.name}</CardHeader>
+            <CardImg src={user.avatar_url} alt='current user profile' />
+            <CardBody>
+                <p>{user.bio}</p>
+                <Button>Github &rarr;</Button>
+            </CardBody>
+            <CardFooter>
+                <span>Followed by {user.followers} people</span>
+                <span>Following {user.following} people</span>
+            </CardFooter>           
+            
+        </Card>
     );
 }
 
