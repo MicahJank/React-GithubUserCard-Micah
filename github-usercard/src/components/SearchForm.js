c039577853e5f736c74e05402a476b6fa7b7f909
MicahJank <MicahJank@users.noreply.github.com>
2019-09-07 20:58:12 -0500

    added some more styling and fixed the layout of the search bar

--- github-usercard/src/components/SearchForm.js
+++ github-usercard/src/components/SearchForm.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
-import { makeStyles, MenuItem, TextField } from '@material-ui/core';
+import { makeStyles, MenuItem, TextField, Box } from '@material-ui/core';
 
 
 const useStyles = makeStyles(theme => ({
     textField: {
         marginLeft: theme.spacing(1),
         marginRight: theme.spacing(1),
-        width: 200,
+        width: 340,
+    },
+    searchBox: {
+        marginBottom: 30,
+        marginTop: 10,
     }
 }))
 
@@ -16,7 +20,9 @@ const SearchForm = () => {
     const classes = useStyles();
 
     return (
-        <TextField id='standard-search' label='Search field' type='search' margin='nomral' className={classes.textField} />
+        <Box className={classes.searchBox} display='flex' justifyContent='center'>
+            <TextField id='standard-search' label='Search field' type='search' margin='nomral' className={classes.textField} />
+        </Box>
     )
 }
 
